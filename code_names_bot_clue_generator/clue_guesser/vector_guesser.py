699924579a1ef7ad8cf558b46bada9a40db46bdc
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-11-05 17:52:26 -0700

    Fix scenarios evaluator

--- code_names_bot_clue_generator/clue_guesser/vector_guesser.py
+++ code_names_bot_clue_generator/clue_guesser/vector_guesser.py
@@ -6,8 +6,17 @@ class VectorGuesser(ClueGuesser):
     def __init__(self, model):
         self._vectors = gensim.downloader.load(model)
 
+    def _similarity(self, word1, word2):
+        word1 = word1.lower()
+        word2 = word2.lower()
+
+        if word1 not in self._vectors.vocab or word2 not in self._vectors.vocab:
+            return 0
+        return self._vectors.similarity(word.lower(), clue.lower())
+
+
     def guess(self, words, clue, num):
-        word_scores = [ (self._vectors.similarity(word.lower(), clue.lower()), word) for word in words ]
+        word_scores = [ (self._similarity(word, clue), word) for word in words ]
         print(word_scores)
         word_scores.sort(reverse=True)
 
